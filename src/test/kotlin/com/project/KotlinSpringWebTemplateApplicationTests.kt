766f60e7169a0552808d0c5ec09f39ae2f0dd321
jellodev <jello0097@hanmail.net>
2025-06-11 13:35:26 +0900

    Initial commit for Kotlin Spring Boot template

---
+++ src/test/kotlin/com/project/KotlinSpringWebTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringWebTemplateApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
